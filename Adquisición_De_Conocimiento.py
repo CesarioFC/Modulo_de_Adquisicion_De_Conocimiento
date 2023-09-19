ea9c70b1baada962c0963519156deda47e4e925e
CesarioFC <143372459+CesarioFC@users.noreply.github.com>
2023-09-18 20:40:07 -0600

    Add files via upload

---
+++ Adquisición_De_Conocimiento.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 20:05:23 2023
+
+@author: cesar
+"""
+
+conocimiento = {
+    "Hola": "Hola, ¿en qué puedo ayudarte?",
+    "¿Cómo estás?": "Estoy aquí para asistirte, ¿en qué puedo ayudarte?",
+    "¿De qué te gustaría hablar?": "Estoy aquí para ayudarte en cualquier tema que desees, ¿en qué estás interesado?"
+}
+
+def obtener_respuesta(pregunta):
+    respuesta = conocimiento.get(pregunta, None)
+    if respuesta:
+        return respuesta
+    else:
+        print("Lo siento, no tengo una respuesta predefinida para esa pregunta.")
+        agregar_nuevo_conocimiento(pregunta)
+
+def agregar_nuevo_conocimiento(pregunta):
+    respuesta = input("Por favor, proporciona una respuesta para esta pregunta: ")
+    conocimiento[pregunta] = respuesta
+    print("¡Gracias por agregar nueva información a mi base de datos!")
+
+# Ejemplo de interacción
+while True:
+    usuario_input = input("Usuario: ")
+    if usuario_input.lower() == "salir":
+        print("¡Hasta luego!")
+        break
+
+    respuesta = obtener_respuesta(usuario_input)
+    print("Finalizado:", respuesta)
